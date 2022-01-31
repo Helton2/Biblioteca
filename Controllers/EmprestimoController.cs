556b86fb336da6e369d96a944bf4a61623f1f2c0
Helton2 <heltonnet125@gmail.com>
2022-01-30 22:51:49 -0300

    Implementacao de melhorias - Etapa 2 e 3 - 01

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -23,29 +23,48 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
-            EmprestimoService emprestimoService = new EmprestimoService();
-            
-            if(viewModel.Emprestimo.Id == 0)
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario))
             {
-                emprestimoService.Inserir(viewModel.Emprestimo);
+                EmprestimoService emprestimoService = new EmprestimoService();
+                
+                if(viewModel.Emprestimo.Id == 0)
+                {
+                    emprestimoService.Inserir(viewModel.Emprestimo);
+                }
+                else
+                {
+                    emprestimoService.Atualizar(viewModel.Emprestimo);
+                }
+                return RedirectToAction("Listagem");
             }
             else
             {
-                emprestimoService.Atualizar(viewModel.Emprestimo);
+                ViewData["mensagem"] = "Por favor, Preencha todos os campos";
+
+                LivroService livroService = new LivroService();
+                EmprestimoService emprestimoService = new EmprestimoService();
+
+                CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+
+                cadModel.Livros = livroService.ListarDisponiveis();
+
+                return View(cadModel);
             }
-            return RedirectToAction("Listagem");
         }
+
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
             Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
+
             if(!string.IsNullOrEmpty(filtro))
             {
                 objFiltro = new FiltrosEmprestimos();
                 objFiltro.Filtro = filtro;
                 objFiltro.TipoFiltro = tipoFiltro;
             }
+
             EmprestimoService emprestimoService = new EmprestimoService();
             return View(emprestimoService.ListarTodos(objFiltro));
         }
@@ -58,7 +77,8 @@ namespace Biblioteca.Controllers
             Emprestimo e = em.ObterPorId(id);
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            cadModel.Livros = livroService.ListarDisponiveis();
+            cadModel.Livros.Add(livroService.ObterPorId(e.LivroId));
             cadModel.Emprestimo = e;
             
             return View(cadModel);
